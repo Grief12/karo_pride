020ac2629d946ea77c151a29fe2f17bef388b6bb
fardanmuhammad <fardanmuhammad0412@gmail.com>
2023-08-02 10:03:01 +0700

    splasjadi

---
+++ android/app/src/main/kotlin/com/example/b_social02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.b_social02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:b_social02/auth/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:b_social02/firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoadScreen(),
+    );
+  }
+}
+
+class LoadScreen extends StatefulWidget {
+  const LoadScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadScreen> createState() => _LoadScreenState();
+}
+
+class _LoadScreenState extends State<LoadScreen> {
+  void initState() {
+    super.initState;
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AuthPage(),
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "assets/images/splash.jpg",
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
