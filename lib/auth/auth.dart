ccbc6eba8ca62bea7b387a48e6cc2e7b842eb28e
nutt99 <140381783+nutt99@users.noreply.github.com>
2023-08-10 16:49:17 +0700

    Update auth.dart

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,5 +1,5 @@
 import 'package:b_social02/auth/login_or_register.dart';
-import 'package:b_social02/pages/home.dart';
+import '../components/Navbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +14,7 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           //user is logged in
           if (snapshot.hasData) {
-            return const HomePage();
+            return const Navbar();
           } else {
             return const LoginOrRegister();
           }
