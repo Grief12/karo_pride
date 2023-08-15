994ffb8955af60a353e40fad4efbf26a174c2231
nutt99 <NataGaming766@gmail.com>
2023-08-15 12:21:53 +0700

    update profil page

---
+++ lib/Api.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class Api {
+  final String urlPost = 'http://127.0.0.1:8000/api/post';
+  final String urlUser = 'http://127.0.0.1:8000/api/user';
+  final String urlChat = 'http://127.0.0.1:8000/api/chat';
+
+  Future getPost() async {
+    final result = await http.get(Uri.parse(urlPost));
+
+    return json.decode(result.body);
+  }
+
+  Future post(String user, String msg, int like) async {
+    final result = await http.post(Uri.parse("http://127.0.0.1:8000/api/post"),
+        body: {"username": user, "message": msg, "likes": like.toString()});
+    return json.decode(result.body);
+  }
+
+  Future profile() async {
+    final result = await http.get(Uri.parse(urlUser));
+
+    return json.decode(result.body);
+  }
+
+  Future registerUser(
+    String email,
+    String username,
+    String password,
+  ) async {
+    final result = await http.post(Uri.parse(urlUser), body: {
+      "email": email,
+      "username": username,
+      "password": password,
+    });
+    return json.decode(result.body);
+  }
+
+  Future fetchChat() async {
+    final result = await http.get(Uri.parse(urlChat));
+
+    return json.decode(result.body);
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:b_social02/components/post.dart';
+import 'package:flutter/material.dart';
+import 'package:b_social02/Api.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //sign out
+  Api api = Api();
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  //Navigate To Profile Page
+  void goToProfile() {
+    //pop menu drawer
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        foregroundColor: Colors.white,
+      ),
+      body: FutureBuilder(
+          future: api.getPost(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data['data'].length,
+                  itemBuilder: (context, index) {
+                    final post = snapshot.data['data'][index];
+                    return FetchPost(post['username'], post['message']);
+                  });
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [CircularProgressIndicator(), Text('Please wait')],
+              ),
+            );
+          }),
+    );
+  }
+}
